0ea3e706dcb2039aa0c267f17e425e58c5b1bb66
Chris Waters <bieoe.ffe@gmail.com>
2025-05-31 16:48:04 -0500

    Use base class functions

--- third-person-controllers/third-person-controller/state-machine/fall.gd
+++ third-person-controllers/third-person-controller/state-machine/fall.gd
@@ -3,16 +3,10 @@ extends ThirdPersonControllerState
 func _do_physics_process(delta: float) -> void:
 	# print("Fall _do_physics_process")
 	if tpc.is_start_dash():
-		finished.emit(DASH, {
-			"delta": delta,
-			"func": "_do_physics_process"
-		})
+		finished.emit(DASH, generate_delta_dictionary(delta, "_do_phsics_process"))
 		return
 	elif tpc.is_on_floor():
-		finished.emit(IWR, {
-			"delta": delta,
-			"func": "_do_physics_process"
-		})
+		finished.emit(IWR, generate_delta_dictionary(delta, "_do_phsics_process"))
 		return
 	
 	tpc._do_fall(delta)
--- third-person-controllers/third-person-controller/state-machine/idle_walk_run.gd
+++ third-person-controllers/third-person-controller/state-machine/idle_walk_run.gd
@@ -3,22 +3,13 @@ extends ThirdPersonControllerState
 func _do_physics_process(delta: float) -> void:
 	# print("IWR _do_physics_process")
 	if tpc.is_start_dash():
-		finished.emit(DASH, {
-			"delta": delta,
-			"func": "_do_physics_process"
-		})
+		finished.emit(DASH, generate_delta_dictionary(delta, "_do_phsics_process"))
 		return
 	elif not tpc.is_on_floor():
-		finished.emit(FALL, {
-			"delta": delta,
-			"func": "_do_physics_process"
-		})
+		finished.emit(FALL, generate_delta_dictionary(delta, "_do_phsics_process"))
 		return
 	elif Input.is_action_pressed("jump"):
-		finished.emit(JUMP, {
-			"delta": delta,
-			"func": "_do_physics_process"
-		})
+		finished.emit(JUMP, generate_delta_dictionary(delta, "_do_phsics_process"))
 		return
 
 	tpc._do_iwr(delta)
